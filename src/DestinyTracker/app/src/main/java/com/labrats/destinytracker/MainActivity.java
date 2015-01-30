1e17db2f7e2485224b578c4ac77b6c9b90109e39
Felipe Raymundo <fprposunifil@gmail.com>
2015-01-30 18:01:39 -0200

    Modificação da tela inicial inicio da criação da raid atheon customizaçao de activity e cards

--- src/DestinyTracker/app/src/main/java/com/labrats/destinytracker/MainActivity.java
+++ src/DestinyTracker/app/src/main/java/com/labrats/destinytracker/MainActivity.java
@@ -1,24 +1,19 @@
 package com.labrats.destinytracker;
 
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+import com.gc.materialdesign.widgets.Dialog;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -39,4 +34,38 @@ public class MainActivity extends ActionBarActivity {
         new MenuInflater(this).inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.main_menu.about: {
+                Dialog dlg = new Dialog(this, getString(R.string.dialog_about_title), "");
+
+                dlg.show();
+
+                //Olhando a documentação, tem um linear layout dentro de um ScrollView. Como o ScrollView aceita somente uma child,
+                //pego ela e tento trocar o conteúdo pelo meu conteúdo que foi criado.
+                ScrollView dlgLayout = (ScrollView)dlg.findViewById(R.id.message_scrollView);
+                LinearLayout l = (LinearLayout)dlgLayout.getChildAt(0);
+                l.removeAllViews();
+                l.addView(getLayoutInflater().inflate(R.layout.dialog_about, null));
+
+                dlg.setButtonCancel(null);
+                dlg.getButtonAccept().setText(getString(R.string.app_dialog_gotit));
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void cardsOnClickListener(View v) {
+        switch(v.getId()) {
+            case R.id.main_card_atheon: {
+                Intent intent = new Intent(MainActivity.this, AtheonActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
